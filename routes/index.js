88a060cd9e2d506a377f0bdf8c0cad9a82010187
lpmi-13 <leskis@gmail.com>
2017-12-01 13:09:16 +0000

    update name

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 
-  res.render('index', { title: 'Grammar Buffet'});
+  res.render('index', { title: 'AnWriting'});
   
 });
 
